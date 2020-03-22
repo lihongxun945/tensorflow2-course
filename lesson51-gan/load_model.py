586ec4bdecc7c888e32f3da456216fa986484981
lihongxun945 <lihongxun945@163.com>
2020-03-22 21:28:08 +0800

    add

--- lesson51-gan/load_model.py
+++ lesson51-gan/load_model.py
@@ -4,7 +4,8 @@ from wgan_train import save_result
 
 generator = tf.keras.models.load_model('./model.tf', compile=True)
 
-z = tf.random.normal([100, 100])
+z = tf.random.normal([1000, 100]) # 10000 张显存不够用啦哈哈
+print(z.shape)
 fake_image = generator(z, training=False)
 img_path = os.path.join('images', 'loaded.png')
-save_result(fake_image.numpy(), 10, img_path, color_mode='P')
+save_result(fake_image.numpy(), 30, img_path, color_mode='P')
--- lesson51-gan/my_wgan_train.py
+++ lesson51-gan/my_wgan_train.py
@@ -7,14 +7,13 @@ from tensorflow import keras
 
 from PIL import Image
 import glob
-from wgan import Generator, Discriminator
+from my_wgan import Generator, Discriminator
 
 from dataset import make_anime_dataset
 
 gpu = tf.config.list_physical_devices('GPU')
 if len(gpu) > 0:
     tf.config.experimental.set_memory_growth(gpu[0], True)
-
 
 def save_result(val_out, val_block_size, image_path, color_mode):
     def preprocess(img):
@@ -46,23 +45,6 @@ def save_result(val_out, val_block_size, image_path, color_mode):
         final_image = np.squeeze(final_image, axis=2)
     Image.fromarray(final_image).save(image_path)
 
-
-def celoss_ones(logits):
-    # [b, 1]
-    # [b] = [1, 1, 1, 1,]
-    # loss = tf.keras.losses.categorical_crossentropy(y_pred=logits,
-    #                                                y_true=tf.ones_like(logits))
-    return - tf.reduce_mean(logits)
-
-
-def celoss_zeros(logits):
-    # [b, 1]
-    # [b] = [1, 1, 1, 1,]
-    # loss = tf.keras.losses.categorical_crossentropy(y_pred=logits,
-    #                                                y_true=tf.zeros_like(logits))
-    return tf.reduce_mean(logits)
-
-
 def gradient_penalty(discriminator, batch_x, fake_image):
     batchsz = batch_x.shape[0]
 
@@ -75,7 +57,7 @@ def gradient_penalty(discriminator, batch_x, fake_image):
 
     with tf.GradientTape() as tape:
         tape.watch([interplate])
-        d_interplote_logits = discriminator(interplate, training=True)
+        d_interplote_logits = discriminator(interplate, True)
     grads = tape.gradient(d_interplote_logits, interplate)
 
     # grads:[b, h, w, c] => [b, -1]
@@ -85,29 +67,28 @@ def gradient_penalty(discriminator, batch_x, fake_image):
 
     return gp
 
+def d_loss_fn(generator, discriminator, batch_x, batch_z, training=None):
+    fake_images = generator(batch_z, training)
 
-def d_loss_fn(generator, discriminator, batch_z, batch_x, is_training):
-    # 1. treat real image as real
-    # 2. treat generated image as fake
-    fake_image = generator(batch_z, is_training)
-    d_fake_logits = discriminator(fake_image, is_training)
-    d_real_logits = discriminator(batch_x, is_training)
+    fake_logits = discriminator(fake_images, training)
+    real_logits = discriminator(batch_x, training)
 
-    d_loss_real = celoss_ones(d_real_logits)
-    d_loss_fake = celoss_zeros(d_fake_logits)
-    gp = gradient_penalty(discriminator, batch_x, fake_image)
+    # 为了让结果正确，显然假图片应该判定为0， 真图片应该判定为1
+    # 那么假图片的判定结果算一下平均值，应该尽量接近0，那么计算平均值就可以当做loss，平均值在0~1之间，显然越小越好
+    fake_loss = tf.reduce_mean(fake_logits)
+    # 同理，那么真图片平均值应该尽量接近1，越大越好，显然他的loss可以直接取负，这样就变成平均值越小越好
+    real_loss = - tf.reduce_mean(real_logits)
+    gp = gradient_penalty(discriminator, batch_x, fake_images)
 
-    loss = d_loss_real + d_loss_fake + 10. * gp
-
+    # 最后返回二者之和，让总共的平均值越小越好
+    loss = fake_loss + real_loss + 10.*gp
     return loss, gp
 
-
-def g_loss_fn(generator, discriminator, batch_z, is_training):
-    fake_image = generator(batch_z, is_training)
-    d_fake_logits = discriminator(fake_image, is_training)
-    loss = celoss_ones(d_fake_logits)
-
-    return loss
+def g_loss_fn(generator, discriminator, batch_z, training=None):
+    fake_images = generator(batch_z, training)
+    fake_logits = discriminator(fake_images, training)
+    # 为了骗过判别器，显然应该是结果尽量接近1， 那么直接取反就能作为loss了
+    return -tf.reduce_mean(fake_logits)
 
 
 def main():
@@ -120,11 +101,9 @@ def main():
     epochs = 3000000
     batch_size = 512
     learning_rate = 0.0005
-    is_training = True
 
     img_path = glob.glob(r'./faces/*.jpg')
     assert len(img_path) > 0
-
     dataset, img_shape, _ = make_anime_dataset(img_path, batch_size)
     print(dataset, img_shape)
     sample = next(iter(dataset))
@@ -134,8 +113,8 @@ def main():
     db_iter = iter(dataset)
 
     generator = None
-    USE_LOADED = False
-    if (USE_LOADED and os.path.exists('./model.tf')):
+    USE_SAVED = True
+    if (USE_SAVED and os.path.exists('./model.tf')):
         print('加载模型，继续上次训练')
         generator = tf.keras.models.load_model('./model.tf', compile=True)
     else:
@@ -149,34 +128,29 @@ def main():
     d_optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=0.5)
 
     for epoch in range(epochs):
-
+        # 判别器每ge epoch进行5次迭代
         for _ in range(5):
-            batch_z = tf.random.normal([batch_size, z_dim])
-            batch_x = next(db_iter)
-
-            # train D
+            batch_z = tf.random.normal([batch_size, z_dim]) # 生成假图片
+            batch_x = next(db_iter) # 加载真图片
             with tf.GradientTape() as tape:
-                d_loss, gp = d_loss_fn(generator, discriminator, batch_z, batch_x, is_training)
+                d_loss, gp = d_loss_fn(generator, discriminator, batch_x, batch_z, True)
             grads = tape.gradient(d_loss, discriminator.trainable_variables)
             d_optimizer.apply_gradients(zip(grads, discriminator.trainable_variables))
 
-        batch_z = tf.random.normal([batch_size, z_dim])
-
+        batch_z = tf.random.normal([batch_size, z_dim])  # 生成假图片
         with tf.GradientTape() as tape:
-            g_loss = g_loss_fn(generator, discriminator, batch_z, is_training)
+            g_loss = g_loss_fn(generator, discriminator, batch_z, True)
         grads = tape.gradient(g_loss, generator.trainable_variables)
         g_optimizer.apply_gradients(zip(grads, generator.trainable_variables))
-
         if epoch % 100 == 0:
-            print(epoch, 'd-loss:', float(d_loss), 'g-loss:', float(g_loss),
-                  'gp:', float(gp))
+            print(epoch, 'd-loss:', float(d_loss), 'g-loss:', float(g_loss), 'gp-loss:', float(gp))
 
             z = tf.random.normal([100, z_dim])
             fake_image = generator(z, training=False)
             img_path = os.path.join('images', 'wgan-%d.png' % epoch)
             save_result(fake_image.numpy(), 10, img_path, color_mode='P')
             # 子类实现的网络保存成h5格式不支持 参见 https://github.com/tensorflow/tensorflow/issues/29545
-            generator.predict(z) # 不调用一下，直接save 会报错，参见这里 https://github.com/tensorflow/tensorflow/issues/31057
+            generator.predict(z)  # 不调用一下，直接save 会报错，参见这里 https://github.com/tensorflow/tensorflow/issues/31057
             generator.save('./model.tf', overwrite=True, save_format="tf")
 
 
