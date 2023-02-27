18da4e6f006aa05759d2252b8a0dbcded671f17f
lihongxun945 <lihongxun945@gmail.com>
2023-02-28 00:03:31 +0800

    feat: 增加dropout示例

--- course2/fashion-mnist/fashion-mnist-dropout.py
+++ course2/fashion-mnist/fashion-mnist-dropout.py
@@ -1,3 +1,4 @@
+# 增加层数，增加了dropout层，试试效果
 import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,9 +10,16 @@ test_image=test_image/255
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(10, activation="softmax"),
 ])
 
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=['acc'])
-history = model.fit(train_image, train_label, epochs=10)
-model.evaluate(test_image, test_label)
+history = model.fit(train_image, train_label, epochs=50, validation_data=(test_image, test_label))
+plt.plot(history.epoch, history.history.get('loss'), label='loss')
+plt.plot(history.epoch, history.history.get('val_loss'), label='val_loss')
+plt.show()
